155ade2be7cc514bd678afe859aceb0bef39a5f6
sodychoe <sodychoe@icloud.com>
2024-06-09 22:18:00 +0900

    init project

---
+++ web-ide/src/test/java/org/goorm/webide/WebIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goorm.webide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
