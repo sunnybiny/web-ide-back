029830c38e530730cb68ecf113defe1ab56ec9af
sodychoe <sodychoe@icloud.com>
2024-06-11 13:20:37 +0900

    feat: create domain entity

---
+++ src/main/java/org/goorm/webide/domain/Project.java
@@ -0,0 +1,24 @@
+package org.goorm.webide.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Project {
+
+  @jakarta.persistence.Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long Id;
+  private String name;
+  private LocalDateTime createdAt;
+  @OneToOne(fetch = FetchType.LAZY)
+  private User createdBy;
+}
