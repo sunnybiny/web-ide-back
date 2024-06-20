f345655e679788652e237d6920946f903c6dda08
sodychoe <sodychoe@icloud.com>
2024-06-20 23:27:48 +0900

    :sparkles: Feat: make repuestDto for sourcode request

---
+++ src/main/java/org/goorm/webide/model/requestDto/Source.java
@@ -0,0 +1,13 @@
+package org.goorm.webide.model.requestDto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonNaming(SnakeCaseStrategy.class)
+public class Source {
+  private String sourceCode;
+}
