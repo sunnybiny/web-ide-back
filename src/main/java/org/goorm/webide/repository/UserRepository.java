f961d289813ee44d4d6127b21995b179b5176c77
sodychoe <sodychoe@icloud.com>
2024-06-24 10:08:38 +0900

    :sparkles:  Feat: add user select query by jwt

--- src/main/java/org/goorm/webide/repository/UserRepository.java
+++ src/main/java/org/goorm/webide/repository/UserRepository.java
@@ -3,8 +3,12 @@ package org.goorm.webide.repository;
 import java.util.Optional;
 import org.goorm.webide.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String name);
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    Optional<User> findByRefreshToken(String refreshToken);
 }
